e29084787230522bef296864f78c3f0043527e25
Kestus <91466473+Kestus@users.noreply.github.com>
2024-09-25 14:39:09 +0300

    initial domain layer

---
+++ App041_Kt_ShoppingList/app/src/main/java/com/kes/app041_kt_shoppinglist/domain/useCases/DeleteShopItemUseCase.kt
@@ -0,0 +1,10 @@
+package com.kes.app041_kt_shoppinglist.domain.useCases
+
+import com.kes.app041_kt_shoppinglist.domain.ShopItem
+import com.kes.app041_kt_shoppinglist.domain.ShopListRepositoryInterface
+
+class DeleteShopItemUseCase (
+    private val repository: ShopListRepositoryInterface
+) {
+    fun deleteShopItem(item: ShopItem) = repository.deleteShopItem(item)
+}
