a92e16308f83bf4dd4188be664b570bedfbf9407
Kestus <91466473+Kestus@users.noreply.github.com>
2024-10-07 17:14:10 +0300

    Revert "Presentation layer"

--- App042_Kt_NumberComposition/app/src/main/java/com/kes/app042_kt_numbercomposition/domain/entity/Question.kt
+++
@@ -1,9 +0,0 @@
-package com.kes.app042_kt_numbercomposition.domain.entity
-
-data class Question(
-    val sum: Int,
-    val visibleNumber: Int,
-    val options: List<Int>
-) {
-    val correctAnswer get() = sum - visibleNumber
-}
