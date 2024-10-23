c08eecb83523fd27cf23baa7402d4ca8308d19b8
Kestus <91466473+Kestus@users.noreply.github.com>
2024-10-23 16:37:10 +0300

    added main activity search field and functionality

--- App045_Kt_Currencies/app/src/main/java/com/kes/app045_kt_currencies/presentation/MainActivity.kt
+++ App045_Kt_Currencies/app/src/main/java/com/kes/app045_kt_currencies/presentation/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.kes.app045_kt_currencies.presentation
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.children
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.kes.app045_kt_currencies.R
 import com.kes.app045_kt_currencies.databinding.ActivityMainBinding
-import com.kes.app045_kt_currencies.databinding.CurrencyCardBinding
-import com.kes.app045_kt_currencies.domain.model.CurrencyItem
 import com.kes.app045_kt_currencies.presentation.adapters.CurrencyListAdapter
+import com.kes.app045_kt_currencies.presentation.viewModel.MainViewModel
+import com.kes.app045_kt_currencies.presentation.viewModel.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
-    private lateinit var viewModel: MainViewModel
+    private val viewModel by lazy {
+        MainViewModelFactory(application).create(MainViewModel::class.java)
+    }
     private lateinit var adapter: CurrencyListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,23 +32,44 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.deleteAll()
         adapter = CurrencyListAdapter()
 
         binding.recyclerView.adapter = adapter
+        viewModel.startUpdateCurrencyWork()
 
-        viewModel.currencyList.observe(this) {
+        observeCurrencyList()
+        setupSearchInputListener()
+
+
+    }
+
+    private fun observeCurrencyList() {
+        viewModel.filteredList.observe(this) {
             adapter.submitList(it)
         }
 
-        viewModel.listIsEmpty.observe(this) {
-            val visibility = if (it) View.GONE
-            else View.VISIBLE
-
+        viewModel.loading.observe(this) {
+            val visibility = if (it) View.VISIBLE
+            else View.GONE
             binding.progressBar.visibility = visibility
         }
+    }
 
 
+    private fun setupSearchInputListener() {
+        binding.etSearch.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.setSearchInput(s)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // TODO("Not yet implemented")
+            }
+        })
     }
 }
